d3d9306305e9a3944a186ee6449027e4d9219395
rodash0902 <rodash0902@gmail.com>
2022-05-06 09:58:02 +0530

    update

--- test/test.py
+++ test/test.py
@@ -3,12 +3,23 @@ import requests
 
 
 def main():
-    files = {
-        'image1': open('test-img.jpg', 'rb')
-    }
-    req = requests.post('http://127.0.0.1:5000/image/edit', files=files, data={"some": "sef"})
+    files = [
+        {'image1': open('test-img.jpg', 'rb')},
+        {'image1': open('img-2.png', 'rb')},
+        {'image1': open('img-3.png', 'rb')},
+    ]
+    for fl in files:
+        req = requests.post('http://127.0.0.1:5000/image/edit', files=fl, data={
+            "style": """
+            image1{
+                blur:10;
+                brightness: 1;
+                resize:  1000,1000;
+            }
+            """
+        })
     
-    print(req.json())
+        print(req.json())
 
 
 if __name__ == '__main__':
